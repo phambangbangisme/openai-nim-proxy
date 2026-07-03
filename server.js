c740f9c5b1b92181892f2823f3a8f5ea669abfeb
phambangbangisme <phambangbangisme@gmail.com>
2026-07-04 04:53:51 +0700

    Update server.js

--- server.js
+++ server.js
@@ -24,7 +24,7 @@ const ENABLE_THINKING_MODE = true; // Set to true to enable chat_template_kwargs
 const MODEL_MAPPING = {
   'gpt-3.5-turbo': 'moonshotai/kimi-k2.6',         
   'gpt-4': 'deepseek-ai/deepseek-v4-pro',             
-  'gpt': 'z-ai/glm-5.1',                      
+  'gpt': 'z-ai/glm-5.2',                      
   'claude-3-opus': 'deepseek-ai/deepseek-r1',    
   'gpt-4-turbo': 'nvidia/llama-3.1-nemotron-ultra-253b-v1',
   'claude-3-sonnet': 'openai/gpt-oss-20b',
